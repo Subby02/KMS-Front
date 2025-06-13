93cc06e6f19b385b3a950092027528bc959fa6c5
HeounTo <lookmeplz@naver.com>
2025-06-13 15:05:23 +0900

    feat

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // src/App.jsx
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage.jsx';
-import RegisterPage from './pages/stdClubPage/RegisterPage.jsx';
-import SearchClubPage from './pages/stdClubPage/SearchClubPage.jsx';
-import EnrolEducationPage from './pages/educationPage/EnrolEducationPage.jsx';
-import SearchEducationPage from './pages/educationPage/SearchEducationPage.jsx';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import RegisterPage from "./pages/stdClubPage/RegisterPage.jsx";
+import SearchClubPage from "./pages/stdClubPage/SearchClubPage.jsx";
+import EnrolEducationPage from "./pages/educationPage/EnrolEducationPage.jsx";
+import SearchEducationPage from "./pages/educationPage/SearchEducationPage.jsx";
+import RegisterTaskPage from "./pages/taskPage/RegisterTaskPage.jsx";
+import SearchTaskPage from "./pages/taskPage/SearchTaskPage.jsx";
 
 const App = () => {
   return (
@@ -16,6 +18,8 @@ const App = () => {
         <Route path="/search" element={<SearchClubPage />} />
         <Route path="/education/enrol" element={<EnrolEducationPage />} />
         <Route path="/education/search" element={<SearchEducationPage />} />
+        <Route path="/task/register" element={<RegisterTaskPage />} />
+        <Route path="/task/search" element={<SearchTaskPage />} />
       </Routes>
     </div>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,30 @@
 // src/pages/Home.jsx
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <div>
       <h1>homepage</h1>
       <div>
-        <Link to="/register"><button style={{ marginRight: '10px' }}>동아리 등록</button></Link>
-        <Link to="/search"><button style={{ marginRight: '10px' }}>동아리 조회</button></Link>
-        <Link to="/education/enrol"><button style={{ marginRight: '10px' }}>교육 등록</button></Link>
-        <Link to="/education/search"><button style={{ marginRight: '10px' }}>교육 검색</button></Link>
+        <Link to="/register">
+          <button style={{ marginRight: "10px" }}>동아리 등록</button>
+        </Link>
+        <Link to="/search">
+          <button style={{ marginRight: "10px" }}>동아리 조회</button>
+        </Link>
+        <Link to="/education/enrol">
+          <button style={{ marginRight: "10px" }}>교육 등록</button>
+        </Link>
+        <Link to="/education/search">
+          <button style={{ marginRight: "10px" }}>교육 검색</button>
+        </Link>
+        <Link to="/task/register">
+          <button style={{ marginRight: "10px" }}>업무 등록</button>
+        </Link>
+        <Link to="/task/search">
+          <button style={{ marginRight: "10px" }}>업무 검색</button>
+        </Link>
       </div>
     </div>
   );
